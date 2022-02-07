332a07b79400f2b78ccbd0ac9a4cece436cf4be6
FaetterP <faettterp@gmail.com>
2022-02-07 17:12:33 +0300

    Добавлена возможность вращать камеру вокруг одной точки
    Управление осуществляется на WASD.

---
+++ Assets/Utilities/MovableCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Utilities
+{
+    class MovableCamera : MonoBehaviour
+    {
+        private float _currentHorizontal = 0;
+        private float _currentVertical = 0;
+
+        private void Update()
+        {
+            float addedHorizontal = Input.GetAxis("Horizontal");
+            float addedVertical = Input.GetAxis("Vertical");
+
+            if (addedHorizontal == 0 && addedVertical == 0)
+                return;
+
+            _currentVertical -= addedVertical;
+            _currentHorizontal -= addedHorizontal;
+            transform.eulerAngles = new Vector3(_currentVertical, _currentHorizontal, 0);
+        }
+    }
+}
