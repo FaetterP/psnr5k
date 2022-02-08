a9d4c9e1fca861edbb71cf1aed46894b9140d2b3
FaetterP <faettterp@gmail.com>
2022-02-08 12:59:44 +0300

    Изменено поведение камеры
    Теперь можно приближать и отдалять камеру.

--- Assets/Utilities/MovableCamera.cs
+++ Assets/Utilities/MovableCamera.cs
@@ -4,11 +4,21 @@ namespace Assets.Utilities
 {
     class MovableCamera : MonoBehaviour
     {
+        [SerializeField] private Transform _center;
+        [SerializeField] private KeyCode _keyZoomIn;
+        [SerializeField] private KeyCode _keyZoomOut;
+
         private float _currentHorizontal = 0;
         private float _currentVertical = 0;
 
         private void Update()
         {
+            if (Input.GetKey(_keyZoomIn))
+                transform.localPosition += new Vector3(0, 0, 0.1f);
+
+            if (Input.GetKey(_keyZoomOut))
+                transform.localPosition += new Vector3(0, 0, -0.1f);
+
             float addedHorizontal = Input.GetAxis("Horizontal");
             float addedVertical = Input.GetAxis("Vertical");
 
@@ -17,7 +27,7 @@ namespace Assets.Utilities
 
             _currentVertical -= addedVertical;
             _currentHorizontal -= addedHorizontal;
-            transform.eulerAngles = new Vector3(_currentVertical, _currentHorizontal, 0);
+            _center.transform.eulerAngles = new Vector3(_currentVertical, _currentHorizontal, 0);
         }
     }
 }
