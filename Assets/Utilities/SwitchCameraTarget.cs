702e1a76277c2f00fe57fef5db79fa8d5525e2b7
FaetterP <faettterp@gmail.com>
2022-02-11 13:07:27 +0300

    Добавлена возможность менять цель для камеры

---
+++ Assets/Utilities/SwitchCameraTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Utilities
+{
+    class SwitchCameraTarget : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _targets;
+        [SerializeField] private Transform _anchor;
+
+        public void ChangeTarget(int index)
+        {
+            _anchor.position = _targets[index].position;
+        }
+
+        public int GetCount()
+        {
+            return _targets.Length;
+        }
+    }
+}
