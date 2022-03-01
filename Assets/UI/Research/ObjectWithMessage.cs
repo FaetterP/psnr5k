841abcd5b24a4c7115d0f41a3db3431ed833467f
FaetterP <faettterp@gmail.com>
2022-03-01 18:21:04 +0300

    Добавлена новая сцена
    На этой сцене можно понажимать и почитать про разные детали.

---
+++ Assets/UI/Research/ObjectWithMessage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.UI.Research
+{
+    class ObjectWithMessage : MonoBehaviour
+    {
+        [SerializeField] private ResearchText _text;
+        [SerializeField] [TextArea] private string _message;
+
+        private void OnMouseDown()
+        {
+            _text.SetText(_message);
+        }
+    }
+}
