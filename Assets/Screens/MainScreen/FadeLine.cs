3b6fc528b621472a28ee3cd167f4ae3905f23593
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-03-29 19:03:29 +0300

    Добавлена ручка ЯРКОСТЬ

---
+++ Assets/Screens/MainScreen/FadeLine.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Switches;
+using UnityEngine;
+
+namespace Assets.Screens.MainScreen
+{
+    [RequireComponent(typeof(LineRenderer))]
+    class FadeLine : MonoBehaviour
+    {
+        [SerializeField] private HandleRotate _handle;
+        [SerializeField] private Color _min;
+        [SerializeField] private Color _max;
+
+        private LineRenderer _thisLineRenderer;
+
+        private void Awake()
+        {
+            _thisLineRenderer = GetComponent<LineRenderer>();
+        }
+
+        private void OnEnable()
+        {
+            _handle.AddListener(ChangeColor);
+        }
+
+        private void OnDisable()
+        {
+            _handle.RemoveListener(ChangeColor);
+        }
+
+        private void ChangeColor(int value)
+        {
+            _thisLineRenderer.material.color = Color.Lerp(_min, _max, value / 100f);
+        }
+    }
+}
