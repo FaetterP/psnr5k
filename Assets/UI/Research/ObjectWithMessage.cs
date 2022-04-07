da9e4985c2c6c8692746797b678232f914e6dc35
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-04-07 15:04:16 +0300

    Добавлены сообщения для блока управления
    Теперь сообщения читаются из файла, а доступ получается по ключу.

--- Assets/UI/Research/ObjectWithMessage.cs
+++ Assets/UI/Research/ObjectWithMessage.cs
@@ -1,15 +1,23 @@
-﻿using UnityEngine;
+﻿using Assets.Utilities;
+using UnityEngine;
 
 namespace Assets.UI.Research
 {
     class ObjectWithMessage : MonoBehaviour
     {
-        [SerializeField] private ResearchText _text;
-        [SerializeField] [TextArea] private string _message;
+        [SerializeField] private ResearchText _researchText;
+        [SerializeField] private string _key;
+
+        private ReadString _string;
+
+        private void Awake()
+        {
+            _string = new ReadString(_key);
+        }
 
         private void OnMouseDown()
         {
-            _text.SetText(_message);
+            _researchText.SetText(_string.GetValue());
         }
     }
 }
