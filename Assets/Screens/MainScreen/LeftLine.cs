cb27229455e611e02b143ac4aac158942c9f3f3c
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-04-14 17:57:10 +0300

    Работает переключатель "стрелочки"
    Он перемещает левую линию от левого края экрана до правого.

---
+++ Assets/Screens/MainScreen/LeftLine.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Switches;
+using UnityEngine;
+
+namespace Assets.Screens.MainScreen
+{
+    class LeftLine : MonoBehaviour
+    {
+        [SerializeField] private HandleRotate _handleWidth;
+        [SerializeField] private HandleRotate _handleArrows;
+
+        [SerializeField] private Vector3 _left;
+        [SerializeField] private Vector3 _right;
+
+        private void OnEnable()
+        {
+            _handleArrows.AddListener(Change);
+        }
+
+        private void OnDisable()
+        {
+            _handleArrows.RemoveListener(Change);
+        }
+
+
+        private void Change(int value)
+        {
+            float v = value / 100f;
+            transform.localPosition = Vector3.Lerp(_left, _right, v);
+        }
+    }
+}
