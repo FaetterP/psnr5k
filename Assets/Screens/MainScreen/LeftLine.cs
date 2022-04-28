97f51c3d910a93c8415873f7e1090f635d1bbadb
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-04-28 16:43:07 +0300

    Сделан переключатель Ширина
    Удалён неиспользуемый приёмопередатчик со сцены (после обновления модели он перестал отображаться).

--- Assets/Screens/MainScreen/LeftLine.cs
+++ Assets/Screens/MainScreen/LeftLine.cs
@@ -8,24 +8,44 @@ namespace Assets.Screens.MainScreen
         [SerializeField] private HandleRotate _handleWidth;
         [SerializeField] private HandleRotate _handleArrows;
 
-        [SerializeField] private Vector3 _left;
-        [SerializeField] private Vector3 _right;
+        [SerializeField] private Vector3 _leftArrows;
+        [SerializeField] private Vector3 _rightArows;
+        [SerializeField] private Vector3 _leftWidth;
+        [SerializeField] private Vector3 _rightWidth;
+
+        private int _arrowsValue;
+        private int _widthValue;
 
         private void OnEnable()
         {
-            _handleArrows.AddListener(Change);
+            _handleArrows.AddListener(SetArrowsValue);
+            _handleWidth.AddListener(SetWidthValue);
         }
 
         private void OnDisable()
         {
-            _handleArrows.RemoveListener(Change);
+            _handleArrows.RemoveListener(SetArrowsValue);
+            _handleWidth.RemoveListener(SetWidthValue);
+        }
+
+        private void SetArrowsValue(int value)
+        {
+            _arrowsValue = value;
+            UpdatePosition();
+        }
+        
+        private void SetWidthValue(int value)
+        {
+            _widthValue = value;
+            UpdatePosition();
         }
 
 
-        private void Change(int value)
+        private void UpdatePosition()
         {
-            float v = value / 100f;
-            transform.localPosition = Vector3.Lerp(_left, _right, v);
+            Debug.Log(_widthValue/40f);
+            transform.localPosition = Vector3.Lerp(_leftArrows, _rightArows, _arrowsValue / 100f);
+            transform.localPosition += Vector3.Lerp(_leftWidth, _rightWidth, _widthValue / 40f);
         }
     }
 }
