0f86febf0dbc55c6d62eff16a8891dc21943eb2c
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-05-28 12:05:59 +0300

    Добавлена асинхронная загрузка сцен
    Компонент Loading отвечает за запуск сцены. У него есть только один метод Load, который принимает сцену. Кнопкам нужно лишь вызвать его.

---
+++ Assets/Utilities/Loading.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Utilities
+{
+    class Loading : MonoBehaviour
+    {
+        [SerializeField] private Text _loadingText;
+        private AsyncOperation _loadCoroutine;
+
+        public void Load(Scenes scene)
+        {
+            StartCoroutine(LoadScene((int)scene));
+        }
+
+        IEnumerator LoadScene(int num)
+        {
+            yield return new WaitForSeconds(1f);
+            _loadCoroutine = SceneManager.LoadSceneAsync(num);
+            while (_loadCoroutine.isDone == false)
+            {
+                int progress = (int)(_loadCoroutine.progress * 100);
+                _loadingText.text = progress + "%";
+                yield return 0;
+            }
+        }
+    }
+}
