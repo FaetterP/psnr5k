57982399dda2a7d8e3385c74de2a83d2dc03ece8
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-12-18 16:50:40 +0300

    Добавлено видео на сцене с обучением

---
+++ Assets/Scripts/UI/Learning/ShowVideoButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+namespace Assets.Scripts.UI.Learning
+{
+    class ShowVideoButton : MonoBehaviour
+    {
+        [SerializeField] private VideoClip _clip;
+        [SerializeField] private RawImage _videoPlayer;
+
+        private void OnMouseDown()
+        {
+            Debug.Log(123);
+            if (_videoPlayer.enabled)
+            {
+                _videoPlayer.enabled = false;
+                return;
+            }
+            else
+            {
+                _videoPlayer.enabled = true;
+                return;
+            }
+        }
+    }
+}
