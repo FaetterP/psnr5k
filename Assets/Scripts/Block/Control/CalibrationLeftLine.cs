5e39d1d7ac5a539636274400eefdf00633061257
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-03-18 14:57:06 +0300

    Добавлена калибровка левой линии
    Второй переключатель К уменьшает и увеличивает высоту левой линии.

---
+++ Assets/Scripts/Block/Control/CalibrationLeftLine.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Switches;
+using UnityEngine;
+
+namespace Assets.Scripts.Block.Control
+{
+    class CalibrationLeftLine : MonoBehaviour
+    {
+        [SerializeField] private HandleRotate _handleK;
+
+        private void OnEnable()
+        {
+            _handleK.AddListener(ChangeValue);
+        }
+
+        private void OnDisable()
+        {
+            _handleK.RemoveListener(ChangeValue);
+        }
+
+        private void ChangeValue(float value)
+        {
+            transform.localScale = new Vector3(1, 1, value / 100);
+        }
+    }
+}
