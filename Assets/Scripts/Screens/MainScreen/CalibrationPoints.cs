a6b6d97e4172e5a5f8c4493a6968c68bf4a88f21
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-03-18 14:30:31 +0300

    Добавлен переключатель для калибровки К
    Этот переключатель калибрует точки, отмечающие километры.

---
+++ Assets/Scripts/Screens/MainScreen/CalibrationPoints.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts.Switches;
+using UnityEngine;
+
+namespace Assets.Scripts.Screens.MainScreen
+{
+    class CalibrationPoints : MonoBehaviour
+    {
+        [SerializeField] private HandleRotate _handleK;
+        [SerializeField] private float _maxOffset;
+
+        private void OnEnable()
+        {
+            _handleK.AddListener(ChangeOffset);
+        }
+
+        private void OnDisable()
+        {
+            _handleK.RemoveListener(ChangeOffset);
+        }
+
+        private void ChangeOffset(float value)
+        {
+            transform.localPosition = new Vector3(0, 0, value * _maxOffset / 100);
+        }
+    }
+}
