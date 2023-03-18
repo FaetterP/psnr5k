6bb156d57c30d3609d87aebd4cac6706c1c7f215
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-03-18 13:22:23 +0300

    Добавлен фокус
    Переключатель "фокус" определяет толщину линий и кругов на экране.

---
+++ Assets/Scripts/Screens/MainScreen/FocusCircle.cs
@@ -0,0 +1,31 @@
+﻿using Assets.Scripts.Switches;
+using UnityEngine;
+
+namespace Assets.Scripts.Screens.MainScreen
+{
+    class FocusCircle : MonoBehaviour
+    {
+        [SerializeField] private HandleRotate _focusHandle;
+        private Vector3 _maxSize;
+
+        private void Awake()
+        {
+            _maxSize = transform.localScale * 2;
+        }
+
+        private void OnEnable()
+        {
+            _focusHandle.AddListener(ChangeFocusValue);
+        }
+
+        private void OnDisable()
+        {
+            _focusHandle.RemoveListener(ChangeFocusValue);
+        }
+
+        private void ChangeFocusValue(float value)
+        {
+            transform.localScale = Vector3.Lerp(Vector3.zero, _maxSize, value / 100);
+        }
+    }
+}
---
+++ Assets/Scripts/Screens/MainScreen/FocusLine.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.Switches;
+using UnityEngine;
+
+namespace Assets.Scripts.Screens.MainScreen
+{
+    [RequireComponent(typeof(LineRenderer))]
+    class FocusLine : MonoBehaviour
+    {
+        [SerializeField] private HandleRotate _focusHandle;
+        private LineRenderer _thisLineRenderer;
+        private float _maxSize;
+
+        private void Awake()
+        {
+            _thisLineRenderer = GetComponent<LineRenderer>();
+            _maxSize = _thisLineRenderer.startWidth * 2;
+        }
+
+        private void OnEnable()
+        {
+            _focusHandle.AddListener(ChangeFocusValue);
+        }
+
+        private void OnDisable()
+        {
+            _focusHandle.RemoveListener(ChangeFocusValue);
+        }
+
+        private void ChangeFocusValue(float value)
+        {
+            float width = Mathf.Lerp(0, _maxSize, value / 100);
+            _thisLineRenderer.startWidth = width;
+            _thisLineRenderer.endWidth = width;
+        }
+    }
+}
