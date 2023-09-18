47f75c3b37c225d7c3b4bfbe205eb50fc6348d0e
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-09-18 14:44:14 +0300

    Добавлена калибровка
    Калибровка К2 позволяет подогнать размер линий к размеру экрана.
    Калибровка К1 позволяет настроить расположение строба дальности.

---
+++ Assets/Scripts/Screens/MainScreen/Calibration2.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.Switches;
+using UnityEngine;
+
+namespace Assets.Scripts.Screens.MainScreen
+{
+    class Calibration2 : MonoBehaviour
+    {
+        [SerializeField] private HandleRotate _calibration;
+        private float _maxSize;
+
+        private void Awake()
+        {
+            _maxSize = transform.localScale.z;
+        }
+
+        private void OnEnable()
+        {
+            _calibration.AddListener(SetValue);
+        }
+
+        private void OnDisable()
+        {
+            _calibration.RemoveListener(SetValue);
+        }
+
+        private void SetValue(float value)
+        {
+            Vector3 scale = transform.localScale;
+            scale.z = _maxSize * value / 100;
+            transform.localScale = scale;
+        }
+    }
+}
--- Assets/Scripts/Screens/MainScreen/RangeStrobe.cs
+++ Assets/Scripts/Screens/MainScreen/RangeStrobe.cs
@@ -8,9 +8,10 @@ namespace Assets.Scripts.Screens.MainScreen
     {
         [SerializeField] private HandleRotate _handleRange;
         [SerializeField] private HandleRotate _handleN;
+        [SerializeField] private HandleRotate _handleK;
         [SerializeField] private HandleStep _handleDelay;
         [SerializeField] private float _minHeight, _maxHeight;
-        private float _valueRange, _valueDelay, _valueN;
+        private float _valueRange, _valueDelay, _valueN, _valueK;
         private MeshRenderer _thisMeshRenderer;
 
         private void Awake()
@@ -23,6 +24,7 @@ namespace Assets.Scripts.Screens.MainScreen
             _handleRange.AddListener(ChangeValueRange);
             _handleDelay.AddListener(ChangeValueDelay);
             _handleN.AddListener(ChangeValueN);
+            _handleK.AddListener(ChangeValueK);
 
             ChangeValueRange(_handleRange.CurrentValue);
         }
@@ -32,6 +34,7 @@ namespace Assets.Scripts.Screens.MainScreen
             _handleRange.RemoveListener(ChangeValueRange);
             _handleDelay.RemoveListener(ChangeValueDelay);
             _handleN.RemoveListener(ChangeValueN);
+            _handleK.RemoveListener(ChangeValueK);
         }
 
         private void ChangeValueRange(float value)
@@ -52,27 +55,35 @@ namespace Assets.Scripts.Screens.MainScreen
             UpdateValues();
         }
 
+        private void ChangeValueK(float value)
+        {
+            _valueK = value;
+            UpdateValues();
+        }
+
         private void UpdateValues()
         {
-            if (_valueRange < 5000 && _valueDelay == 0)
+            float range = _valueK * (_valueRange - 1000) / 100f + _valueN + 1000;
+
+            if (range < 5000 && _valueDelay == 0)
             {
-                float z = Mathf.Lerp(_minHeight, _maxHeight, 1f * _valueRange / 5000 + _valueN / 100 / 2.5f);
+                float z = Mathf.Lerp(_minHeight, _maxHeight, 1f * range / 5000 + _valueN / 100 / 2.5f);
 
                 Vector3 position = transform.localPosition;
                 position.z = z;
                 transform.localPosition = position;
             }
-            else if (_valueRange < 10000 && _valueDelay == 1)
+            else if (range < 10000 && _valueDelay == 1)
             {
-                float z = Mathf.Lerp(_minHeight, _maxHeight, 1f * (_valueRange - 5000) / 5000 + _valueN / 100 / 2.5f);
+                float z = Mathf.Lerp(_minHeight, _maxHeight, 1f * (range - 5000) / 5000 + _valueN / 100 / 2.5f);
 
                 Vector3 position = transform.localPosition;
                 position.z = z;
                 transform.localPosition = position;
             }
-            else if (_valueRange < 15000 && _valueDelay == 2)
+            else if (range < 15000 && _valueDelay == 2)
             {
-                float z = Mathf.Lerp(_minHeight, _maxHeight, 1f * (_valueRange - 10000) / 5000 + _valueN / 100 / 2.5f);
+                float z = Mathf.Lerp(_minHeight, _maxHeight, 1f * (range - 10000) / 5000 + _valueN / 100 / 2.5f);
 
                 Vector3 position = transform.localPosition;
                 position.z = z;
