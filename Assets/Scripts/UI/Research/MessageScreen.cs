93f02ff520c7b1a1ac520cf7168bf7c9fcdab3c7
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-12-02 15:15:51 +0300

    Добавлено изменение размера изображения
    Теперь Image автоматически меняет размер для любых спрайтов.
    • Добавлены некоторые спрайты для примера.

--- Assets/Scripts/UI/Research/MessageScreen.cs
+++ Assets/Scripts/UI/Research/MessageScreen.cs
@@ -8,15 +8,23 @@ namespace Assets.Scripts.UI.Research
     {
         private static MessageScreen s_instance;
 
+        [SerializeField] private Image _mask;
         [SerializeField] private Image _image;
         [SerializeField] private Text _name;
         [SerializeField] private Text _description;
+
+        private float widthUI;
+        private float heightUI;
+        float factorUI;
 
         public static MessageScreen Instance => s_instance;
 
         private void Awake()
         {
             s_instance = this;
+            widthUI = _mask.rectTransform.rect.width;
+            heightUI = _mask.rectTransform.rect.height;
+            factorUI = widthUI / heightUI;
         }
 
         private void Start()
@@ -36,6 +44,16 @@ namespace Assets.Scripts.UI.Research
             _image.sprite = sprite;
             _name.text = Messages.getValue($"{key}.name");
             _description.text = Messages.getValue($"{key}.desc");
+
+            float widthSprite = sprite.rect.width;
+            float heighSprite = sprite.rect.height;
+            float factorSprite = widthSprite / heighSprite;
+
+            float newWidth = (factorSprite > factorUI) ? widthUI : (factorSprite * widthUI / factorUI);
+            float newHeight = (factorSprite > factorUI) ? (heightUI * factorUI / factorSprite) : heightUI;
+
+            _image.rectTransform.sizeDelta = new Vector2(newWidth, newHeight);
+            _mask.rectTransform.sizeDelta = new Vector2(newWidth, newHeight);
         }
     }
 }
